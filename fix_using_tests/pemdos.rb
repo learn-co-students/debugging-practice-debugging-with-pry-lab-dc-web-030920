2b5f9dafc449e59078a371c54bf03670e7366bb7
Daniel Succar <danielsuccar@icloud.com>
2020-01-29 15:25:06 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,13 @@
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
-  else
-  string
+    length = 10
+    i =0
+    while i<length do
+      string = 's' + string
+        i += 1
+    end  
+      return string
   end
+    string
 end
