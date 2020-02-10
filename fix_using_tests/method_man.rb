86db2ec01aa437608ccb599dbd232d884e595b6d
Ricardo L <rilax92@gmail.com>
2020-02-10 23:46:41 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,16 @@
 require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  # player1 = "Rocky"
+  # player2 = "Bullwinkle"
+  greeting = "Hello #{player1} & #{player2}"
+  
+  greeting
 end
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+def play_game(player1, player2)
+  player1 = "Jay-Z"
+  player2 = "Beyonce"
+  truth = "#{player1} is better than #{player2}"
+  truth
 end
